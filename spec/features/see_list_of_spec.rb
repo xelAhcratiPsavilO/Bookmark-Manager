dc5920cbcf6ad70a5d66ba226618b6bde20efb5a
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-03-12 15:43:49 +0000

    Red phase for see_list_of_spec.rb

---
+++ spec/features/see_list_of_spec.rb
@@ -0,0 +1,9 @@
+feature 'Viewing links' do
+  scenario ' A user can see links' do
+    visit '/'
+
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_content "http://www.facebook.com"       
+  end
+end
