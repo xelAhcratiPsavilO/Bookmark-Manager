8334cf0afc3c4ad42054987a2578b9e8d5669762
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-03-13 12:38:35 +0000

    Add DataBase

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 ruby '2.5.0'
 
 gem 'capybara'
+gem 'pg'
 gem 'rerun'
 gem 'rspec'
 gem 'sinatra'
--- lib/link.rb
+++ lib/link.rb
@@ -1,10 +1,10 @@
+require 'pg'
+
 class Link
 
   def self.all
-    [
-      "http://www.makersacademy.com",
-      "http://www.google.com",
-      "http://www.facebook.com"
-    ]
+    con = PG.connect :dbname => 'bookmark_manager'
+    rs =  con.exec "SELECT * FROM links"
+    rs.map { |link| link['url'] }
   end
 end
